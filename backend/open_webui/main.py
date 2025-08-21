d6cb45c619bd50d0877302a34c5d0879485b0100
Detin_Tech <benjamin.hudson4@gmail.com>
2025-08-21 17:33:52 -0400

    feat: sync users from supabase

--- backend/open_webui/main.py
+++ backend/open_webui/main.py
@@ -84,11 +84,12 @@ from open_webui.routers import (
     evaluations,
     tools,
     users,
-    sync_users,
     utils,
     custom,
     scim,
 )
+
+from open_webui.routes.api import sync_users as supabase_sync_users
 
 from open_webui.routers.retrieval import (
     get_embedding_function,
@@ -1222,7 +1223,7 @@ app.include_router(configs.router, prefix="/api/v1/configs", tags=["configs"])
 
 app.include_router(auths.router, prefix="/api/v1/auths", tags=["auths"])
 app.include_router(users.router, prefix="/api/v1/users", tags=["users"])
-app.include_router(sync_users.router, prefix="/api", tags=["sync-users"])
+app.include_router(supabase_sync_users.router, prefix="/api", tags=["sync-users"])
 
 
 app.include_router(channels.router, prefix="/api/v1/channels", tags=["channels"])
